1508fa247c8e369e66cc8b77bf76e9437304ab86
Jamie Pham <jtkp66@gmail.com>
2019-01-26 17:34:07 -0800

    initial commit. Able to make calls to youtube API and recieve a list from search term

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+
+export default class App extends Component {
+  onTermSubmit = (term) => {
+    console.log(term);
+    youtube.get('./search', {
+      params: {
+        q: term
+      }
+    });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+      </div>
+    );
+  }
+}
