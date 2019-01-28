e38a688d9e95013f1607b83edead0f9300e8e539
Jamie Pham <jtkp66@gmail.com>
2019-01-28 11:23:05 -0800

    Added Video Detail component, and VideoItem component to render each video

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 export default class App extends Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async (term) => {
     console.log(term);
@@ -17,11 +18,17 @@ export default class App extends Component {
     this.setState({ videos: response.data.items });
   };
 
+  onVideoSelect = (video) => {
+    console.log('From the App component', video);
+    this.setState({ selectedVideo: video });
+  };
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
       </div>
     );
   }
