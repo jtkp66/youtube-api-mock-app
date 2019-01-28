d66873b37ad34cb5513274e01d0bb06f82545cd8
Jamie Pham <jtkp66@gmail.com>
2019-01-28 11:47:13 -0800

    Added iFrame to video detail component and fixed styling for video list items

--- src/components/App.js
+++ src/components/App.js
@@ -7,15 +7,22 @@ import VideoDetail from './VideoDetail';
 export default class App extends Component {
   state = { videos: [], selectedVideo: null };
 
+  componentDidMount() {
+    this.onTermSubmit('big waves')
+  }
+
   onTermSubmit = async (term) => {
     console.log(term);
-   const response = await youtube.get('./search', {
+    const response = await youtube.get('./search', {
       params: {
         q: term
       }
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   };
 
   onVideoSelect = (video) => {
@@ -27,8 +34,16 @@ export default class App extends Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
