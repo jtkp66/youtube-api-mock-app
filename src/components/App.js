79b16bfad22bbae4d13dc96b814f208ab3ec3e82
Jamie Pham <jtkp66@gmail.com>
2019-01-28 10:31:06 -0800

    Added Video List component with props object from App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 
 export default class App extends Component {
-  onTermSubmit = (term) => {
+  state = { videos: [] };
+
+  onTermSubmit = async (term) => {
     console.log(term);
-    youtube.get('./search', {
+   const response = await youtube.get('./search', {
       params: {
         q: term
       }
     });
+
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
